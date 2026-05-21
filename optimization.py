defa00bcd228245645e74014038ca7296f7642b4
JavierErdosio <erdosiojavier@gmail.com>
2026-05-21 10:12:04 -0300

    Updated to work with previous changes

--- optimization.py
+++ optimization.py
@@ -16,14 +16,26 @@ trials = 500
 
 
 def optimization(trial):
+    #Extras
+    sun = True #If you have a passive sensor set to True
+    terrritoryResolution = 5000 #[meters] distance between dots
+    EPSG = 3857 #EPSG code that better represent the region of study - 3857 is for Web Mercator
+
     #Variables
     SatCount = trial.suggest_int('SatCount', 5, 40)
     orbPlaneCount = trial.suggest_int('orbPlaneCount', 3, 10)
+    
     e = trial.suggest_float('e', 0.0, 0.5, step=0.001) #[-] Eccentricity
+    
     hp = trial.suggest_int('hp', 600, 1700,step=10) #[km] Perigee
+    
+    inc = trial.suggest_float('inc',-90,-41,step=0.1) #[deg] Inclination
+    inc = np.deg2rad(inc) #[rad] inclination
+    
+    spaced = trial.suggest_int("Delta theta",10,360,step=5) #[deg]
+    spaced = np.deg2rad(spaced) #[rad]
 
     #Constants
-    inc = np.deg2rad(-63.4394882) #[rad] Inclination
     omega = np.deg2rad(270) #[rad] Argumento del perigeo
     Re = 6378
     Rmax = 1700 + Re
@@ -31,8 +43,9 @@ def optimization(trial):
         raise optuna.TrialPruned() #Optuna abort
 
     #Time data
-    hours = 23.93446944 #Sidereal day 
-    steps = 30*60 #30 seconds and 60 frames per second
+    simDays = 30
+    hours = 23.93446944*simDays #Sidereal day 
+    steps = 30*60*(2*simDays) #30 seconds and 60 frames per second
 
     #Satellite
     cameraAngle = 4.46 #[deg]
@@ -41,7 +54,7 @@ def optimization(trial):
     RAAN = np.linspace(0, 2*np.pi, orbPlaneCount+1)
     RAAN = RAAN[0:-1]
 
-    theta = np.linspace(0, 2*np.pi, SatCount+1)
+    theta = np.linspace(0, spaced, SatCount+1)
     theta = theta[0:-1]
 
     #Initial state vector
@@ -63,7 +76,7 @@ def optimization(trial):
     olatlong = TwoDimPlot(hours,steps,Orbits,False)
 
     #Max and min revisit time
-    mrt,cov =  revisitTime(olatlong,cameraAngle,Orbits,hours/steps,True)
+    mrt,cov =  revisitTime(olatlong,cameraAngle,Orbits,hours/steps,False,sun,terrritoryResolution,EPSG)
 
     if cov < 95:
         raise optuna.TrialPruned()
@@ -74,7 +87,8 @@ def optimization(trial):
 
 
 #Optuna Optimizer
-opt = optuna.create_study(direction='minimize',storage="mysql+pymysql://%s:%s@%s:%s/optuna_db"%(dataBaseUser,dataBasePassword,dataBaseURL,dataBasePort),study_name=studyName,load_if_exists=True)
+samp = optuna.samplers.TPESampler(n_startup_trials=300)
+opt = optuna.create_study(sampler=samp,direction='minimize',storage="mysql+pymysql://%s:%s@%s:%s/optuna_db"%(dataBaseUser,dataBasePassword,dataBaseURL,dataBasePort),study_name=studyName,load_if_exists=True)
 opt.optimize(optimization, n_trials=trials,gc_after_trial=True)
 
 #Results
